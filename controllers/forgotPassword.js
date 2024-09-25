a0ad06e40a0f4a6f9c2cc49b9f03d6788e43aa53
WaveDevLab <158244998+WaveDevLab@users.noreply.github.com>
2024-09-25 22:28:36 +0700

    Update forgotPassword.js

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -8,7 +8,7 @@ import bcrypt from "bcryptjs";
 import dbPromise from "../dbPromise.js";
 import db from "../db.js"
 
-const baseURL = "https://it-activity-npru.vercel.app/";
+const baseURL = "it-activity-npru.vercel.app";
 
 // Request Password Reset
 export const forgotPassword = async (req, res) => {
