acf2b7ac11d5ba38e72079eff8a40162f63be3e8
Karan857 <73943140+Karan857@users.noreply.github.com>
2024-10-02 07:57:45 +0700

    Update blockchain.js

--- controllers/blockchain.js
+++ controllers/blockchain.js
@@ -1,35 +1,44 @@
-import db from '../db.js'
+import db from "../db.js";
 
 export const actByStatus = (req, res) => {
-    const sql = `
+  const sql = `
         SELECT 
-            a.*,t.*,s.*
-        FROM
-            activity a
-        LEFT JOIN teacher t ON a.t_ID = t.t_ID
-        LEFT JOIN section s ON t.t_ID = s.t_ID
-        WHERE
-            a.act_status = "เปิดลงทะเบียน"
+    a.*, 
+    t.*, 
+    s.*, 
+    COUNT(p.std_ID) AS act_numStdReserve
+FROM
+    activity a
+LEFT JOIN teacher t ON a.t_ID = t.t_ID
+LEFT JOIN section s ON t.t_ID = s.t_ID
+LEFT JOIN participate p ON a.act_ID = p.act_ID -- Assuming 'participate' table for counting reserved students
+WHERE
+    a.act_status = 'เปิดลงทะเบียน'
+    GROUP BY 
+    a.act_ID, t.t_ID;
+
+
+
     `;
 
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.error('Database error:', err);
-            return res.status(500).json({
-                error: 'An error occurred while fetching data'
-            });
-        }
-        if (result.length === 0) {
-            return res.status(404).json({
-                message: "Data not found"
-            });
-        }
-        return res.json(result);
-    });
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error("Database error:", err);
+      return res.status(500).json({
+        error: "An error occurred while fetching data",
+      });
+    }
+    if (result.length === 0) {
+      return res.status(404).json({
+        message: "Data not found",
+      });
+    }
+    return res.json(result);
+  });
 };
 
 export const par = (req, res) => {
-    const sql = `
+  const sql = `
         SELECT 
             p.*,st.*,s.*
         FROM
@@ -38,18 +47,18 @@ export const par = (req, res) => {
         LEFT JOIN section s ON st.sec_ID = s.sec_ID
     `;
 
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.error('Database error:', err);
-            return res.status(500).json({
-                error: 'An error occurred while fetching data'
-            });
-        }
-        if (result.length === 0) {
-            return res.status(404).json({
-                message: "Data not found"
-            });
-        }
-        return res.json(result);
-    });
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error("Database error:", err);
+      return res.status(500).json({
+        error: "An error occurred while fetching data",
+      });
+    }
+    if (result.length === 0) {
+      return res.status(404).json({
+        message: "Data not found",
+      });
+    }
+    return res.json(result);
+  });
 };
